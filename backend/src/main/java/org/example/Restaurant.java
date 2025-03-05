4dd6eaf62a6f2563125c7af79c6f376e71c64913
Ellie Byrne <elliebyrne@carthus.local>
2025-03-05 11:39:54 +0000

    Restructure project: move backend files into backend/ folder

---
+++ backend/src/main/java/org/example/Restaurant.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "restaurants")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Restaurant {
+    @Id
+    @JsonSerialize(using = ObjectIdSerializer.class)
+    private ObjectId id; // Changed from _id
+    private String name;
+    private String type;
+    private String link;
+    private String storePhoto;
+    private String review;
+}
