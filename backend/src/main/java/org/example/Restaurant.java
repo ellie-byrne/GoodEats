705cde2576bfc550323b5e4ae10d6ccf6b74b9a5
Tanvi <tanvidav6@gmail.com>
2025-03-23 22:49:20 +0000

    added more debugging as restaurants are not showing

--- backend/src/main/java/org/example/Restaurant.java
+++ backend/src/main/java/org/example/Restaurant.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "restaurants")
+@Document(collection = "CSV data")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
