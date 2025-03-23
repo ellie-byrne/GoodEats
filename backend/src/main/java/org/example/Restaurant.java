c8a12790952f2c3e43b2186894445f985009be71
Tanvi <tanvidav6@gmail.com>
2025-03-23 18:11:48 +0000

    updated mongodb variable name matches

--- backend/src/main/java/org/example/Restaurant.java
+++ backend/src/main/java/org/example/Restaurant.java
@@ -6,6 +6,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "restaurants")
 @Data
 @AllArgsConstructor
@@ -14,9 +16,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Restaurant {
     @Id
     @JsonSerialize(using = ObjectIdSerializer.class)
-    private ObjectId id; // Changed from _id
-    private String name;
-    private String type;
+    private ObjectId id;
+
+    private String Borough;
+    private String Name;
+    private String Category;
+
+    //for future use
     private String link;
     private String storePhoto;
     private String review;
