dd59396fbdd9f88ca1c3b4a441a06bad338dbb0f
Ellie Byrne <108229155+ellie-byrne@users.noreply.github.com>
2025-05-02 01:34:18 +0100

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -23,7 +23,7 @@ resource "google_compute_firewall" "jenkins-firewall" {
   
   allow {
     protocol = "tcp"
-    ports    = ["22", "8080", "8081"]
+    ports    = ["22", "8080", "5000"]
   }
   
   source_ranges = ["0.0.0.0/0"]
